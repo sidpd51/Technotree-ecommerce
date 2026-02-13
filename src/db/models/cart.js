3ffccd2ed685b181288ee50a21fcbbf264cdaebd
sidpd51 <sidpd510@gmail.com>
2026-02-13 21:13:57 +0530

    feat: add Cart model and migration for managing user shopping carts

---
+++ src/db/models/cart.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Cart.init({
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    totalAmount: { type: DataTypes.DECIMAL(10, 2), allowNull: false, defaultValue: 0, validate: { min: 0 } }
+  }, {
+    sequelize,
+    modelName: 'Cart',
+  });
+  return Cart;
+};
