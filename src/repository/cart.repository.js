4168a1f6993cd2c398f70bb56588e0e37d1710d7
Soumyarout2002 <soumyarout002@gmail.com>
2026-02-27 00:02:22 +0530

    Add new error handler logic

--- src/repository/cart.repository.js
+++ src/repository/cart.repository.js
@@ -1,15 +1,15 @@
-const { CartModel } = require("../db/models")
+const { Cart } = require("../db/models")
 
 const getCartRepository = async (userId) => {
-    return await CartModel.findOne({ where: { userId } })
+    return await Cart.findOne({ where: { userId } })
 }
 
 const createCartRepository = async (userId) => {
-    return await CartModel.create({ userId, totalAmount: 0 })
+    return await Cart.create({ userId, totalAmount: 0 })
 }
 
 const updateCartTotalAmountRepository = async (cartId, totalAmount) => {
-    return await CartModel.update({ totalAmount }, { where: { id: cartId } })
+    return await Cart.update({ totalAmount }, { where: { id: cartId } })
 }
 
 module.exports = {
